f39c32115e09a0a0a35bb57cbce28eda9ed65442
ajaykadus <ajay.kadus@gmail.com>
2021-02-11 21:41:18 -0800

    Fix for local dev server

--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,30 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import schema from "./Schema";
+import { Express } from "apollo-server-express/node_modules/@types/express/node_modules/@types/express-serve-static-core";
 
 const initializeServer = () => {
-    const app = express();
-
+    const app = express()
     const server = new ApolloServer({
         schema,
         playground: true,
     });
 
-    app.use("*", cors());
-
-    server.applyMiddleware({ app, path: "/graphql" });
+    app.use("*", cors())
+    server.applyMiddleware({ app, path: "/graphql" })
 
     return app
 }
 
+const startDevServer = (app: Express) => {
+    app.listen({ port: 8000 }, () => {
+        console.log("Apollo Server on http://localhost:8000/graphql")
+    })
+}
+
 if (process.env.NODE_ENV === "dev") {
-    initializeServer()
+    const app = initializeServer()
+    startDevServer(app)
 }
 
 export default initializeServer
