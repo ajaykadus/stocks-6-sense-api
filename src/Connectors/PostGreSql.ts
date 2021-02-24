d25c9dc72c65fb47c661cf999f787d026dd669d8
ajaykadus <ajay.kadus@gmail.com>
2021-02-23 20:56:58 -0800

    Differntiating from local to containerized progres

--- src/Connectors/PostGreSql.ts
+++ src/Connectors/PostGreSql.ts
@@ -3,13 +3,18 @@ import { EPERM } from 'constants';
 import pgPromise from 'pg-promise'
 
 const pgp = pgPromise({ capSQL: true }); // Empty object means no additional config required
+const isDev = process.env.NODE_ENV === 'dev'
+const host = isDev ? process.env.POSTGRES_HOST_LOCAL : process.env.
+POSTGRES_HOST
+const user = isDev ? process.env.POSTGRES_USER_LOCAL : process.env.POSTGRES_USER
+const password = isDev ? process.env.POSTGRES_PASSWD_LOCAL : process.env.POSTGRES_PASSWORD
 
 const config = {
-    host: process.env.POSTGRES_HOST,
+    host,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD
+    user,
+    password
 } as any
 
 const db = pgp(config)
